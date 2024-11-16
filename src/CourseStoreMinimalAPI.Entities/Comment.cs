28bca205692d66d4a0dcd7e4b748e476c5a4edff
oroumand <ar.oroumand@gmail.com>
2024-11-16 09:08:46 +0330

    Start Develop using .NET 9

--- src/CourseStoreMinimalAPI.Entities/Comment.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CourseStoreMinimalAPI.Entities
-{
-    public sealed class Comment : BaseEntity<int>
-    {
-        public string Body { get; set; } = null!;
-        public int CourseId { get; set; }
-    }
-}
